6a3c701f27bae632d61a512cbb52a63df2eb3249
Fxllo <dallagliofilippo32@gmail.com>
2023-03-18 12:45:23 +0100

    Update blur script
    Toggle blur adn title on click (not functioning very well)

--- progetto Tepsit 2023/js/script.js
+++ progetto Tepsit 2023/js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    
+
+    var gameContainer = $(".gameContainer");
+
+    //blur the div gameContainer on load 
+    gameContainer.css("filter", "blur(10px)");
+
+    //onclick toogle blur
+    gameContainer.click(function () {
+        $(this).css("filter", "blur(0px)");
+        $("#imgTitle").fadeOut({
+            opacity: "0",
+        }, 1000);
+    });
+
 });
