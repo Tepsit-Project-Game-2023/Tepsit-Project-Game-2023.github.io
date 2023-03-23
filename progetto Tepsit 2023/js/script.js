23b4d827bc06cc177ffab0ee3c8919d38b9e93c0
Fxllo <dallagliofilippo32@gmail.com>
2023-03-23 17:24:01 +0100

    added some function
    1) Added animations
    2)Added all the game buttons and GUI
    3)Added the resources bar
    4) fill the bar with the corrispodent % of the total (TODO)

--- progetto Tepsit 2023/js/script.js
+++ progetto Tepsit 2023/js/script.js
@@ -1,11 +1,18 @@
 $(document).ready(function () {
+    //prewiew images
     var gameContainer = $(".gameContainer");
     var imgTitle = $("#imgTitle");
     var title = $("#title");
+
+    //game buttons
     var settingButton = $("#settingButton");
+    var buildButton = $("#buildButton");
+    var resourcesBarFiller = $("#resourcesBarFiller");
+    var opacityBackgroundBarFiller = $("#opacityBackgroundBarFiller");
     
     //blur the gameContainer on load 
     gameContainer.css("filter", "blur(5px)");
+    //hide all the game buttons
     hideAllGameButtons();
 
     //onclick toogle blur
@@ -45,13 +52,37 @@ $(document).ready(function () {
         showAllGameButtons();
     });
 
+    //hide all the game buttons
     function hideAllGameButtons() {
         settingButton.css("opacity", "0");
+        buildButton.css("opacity", "0");
+        resourcesBarFiller.css("opacity", "0");
+        opacityBackgroundBarFiller.css("opacity", "0");
     }
 
+    //show all the game buttons
     function showAllGameButtons() {
-        settingButton.fadeIn({
+        settingButton.animate({
             opacity: "1",
-        }, 1000);
+        }, 500);
+        buildButton.animate({
+            opacity: "1",
+        }, 500);
+        resourcesBarFiller.animate({
+            opacity: "1",
+        }, 500);
+        opacityBackgroundBarFiller.animate({
+            opacity: "0.2",
+        }, 500);
     }
+    
+
+    //game functions
+    var resources = 20000;
+    const RESOURCESMAX = 20000;
+
+    //calculate the resources
+    // function calculatePercentuageFiler () {
+    //     return (resources / RESOURCESMAX) * 100;
+    // }
 });
