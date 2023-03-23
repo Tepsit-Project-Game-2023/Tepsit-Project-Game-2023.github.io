5b1015425d85761fd120bd24c890d1e503af5a9f
Fxllo <dallagliofilippo32@gmail.com>
2023-03-23 09:44:29 +0100

    add the title pulse animation
    Added the keyframe animation with the titlle
    Added the funtion to hide / show the game buttons (not finished)

--- progetto Tepsit 2023/js/script.js
+++ progetto Tepsit 2023/js/script.js
@@ -1,22 +1,57 @@
 $(document).ready(function () {
     var gameContainer = $(".gameContainer");
     var imgTitle = $("#imgTitle");
+    var title = $("#title");
+    var settingButton = $("#settingButton");
     
-    //blur the canvas gameContainer on load 
+    //blur the gameContainer on load 
     gameContainer.css("filter", "blur(5px)");
+    hideAllGameButtons();
 
     //onclick toogle blur
     gameContainer.click(function () {
         $(this).css("filter", "blur(0px)");
-        $("#imgTitle").fadeOut({
+        imgTitle.fadeOut({
             opacity: "0",
         }, 1000);
+        title.fadeOut({
+            opacity: "0",
+        }, 1000);
+
+        showAllGameButtons();
     });
 
     imgTitle.click(function () {
-        $(gameContainer).css("filter", "blur(0px)");
+        gameContainer.css("filter", "blur(0px)");
         $(this).fadeOut({
             opacity: "0",
         }, 1000);
+        title.fadeOut({
+            opacity: "0",
+        }, 1000);
+
+        showAllGameButtons();
     });
+    
+    title.click(function () {
+        gameContainer.css("filter", "blur(0px)");
+        imgTitle.fadeOut({
+            opacity: "0",
+        }, 1000);
+        $(this).fadeOut({
+            opacity: "0",
+        }, 1000);
+
+        showAllGameButtons();
+    });
+
+    function hideAllGameButtons() {
+        settingButton.css("opacity", "0");
+    }
+
+    function showAllGameButtons() {
+        settingButton.fadeIn({
+            opacity: "1",
+        }, 1000);
+    }
 });
